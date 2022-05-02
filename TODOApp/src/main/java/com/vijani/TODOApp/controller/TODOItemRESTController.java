f35f6ac49ebe3c9c60552134c69250fae894bcf0
vijanipiyawardana <vijanipiyawardana@gmail.com>
2022-05-02 15:44:33 +0530

    added response entity for return type.

--- TODOApp/src/main/java/com/vijani/TODOApp/controller/TODOItemRESTController.java
+++ TODOApp/src/main/java/com/vijani/TODOApp/controller/TODOItemRESTController.java
@@ -3,6 +3,7 @@ package com.vijani.TODOApp.controller;
 import com.vijani.TODOApp.entity.TODOItem;
 import com.vijani.TODOApp.service.TODOItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,15 +18,17 @@ public class TODOItemRESTController {
     private TODOItemService todoItemService;
 
     @PostMapping("/todoSave")
-    public String todoSave(@RequestBody TODOItem todoItem){
+    public ResponseEntity<String>  todoSave(@RequestBody TODOItem todoItem){
         String output = todoItemService.todoSave(todoItem);
-        return output;
+        ResponseEntity<String> response = ResponseEntity.accepted().body(output);
+        return response;
     }
 
     @GetMapping("/todoGet")
-    public List<TODOItem> todoGet(){
+    public ResponseEntity<List<TODOItem>> todoGet(){
         List<TODOItem> todoItems = todoItemService.todoGetAll();
-        return todoItems;
+        ResponseEntity<List<TODOItem>> response = ResponseEntity.ok(todoItems);
+        return response;
     }
 
 }
