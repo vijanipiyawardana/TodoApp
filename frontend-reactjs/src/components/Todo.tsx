c1582270e90d522d298beba6c1bb3ba3932adee1
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-15 01:19:41 +0200

    started using react-query

--- frontend-reactjs/src/components/Todo.tsx
+++ frontend-reactjs/src/components/Todo.tsx
@@ -1,5 +1,34 @@
+import { useQuery } from "@tanstack/react-query"
+import { TodoCard } from "./TodoCard";
+import axios from 'axios';
+
+export type todoType = {
+    id: string;
+    title: string;
+    addedDate: string;
+    status: string;
+}
+
 export const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
+
+    const fetchTodos = () => {
+        axios.get("http://localhost:8080/api/todo")
+    }
+
+    const { data: todos, isLoading } = useQuery({
+        queryFn: () => fetchTodos(),
+        queryKey: ["todos"]
+    });
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    
+    return (
+        <div>
+            { todos?.map((todo) => {
+                return <TodoCard key={todo.id} todo={todo}/>
+            })}
+        </div>
+    )
 }
