5e76c6bbda34952f005fff2219893616a31bdf80
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-15 00:43:42 +0200

    import queryClient and wrapped the App from it

--- frontend-reactjs/src/App.tsx
+++ frontend-reactjs/src/App.tsx
@@ -1,9 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Todo } from './components/Todo'
 import './App.css'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-    </>
+    <QueryClientProvider client = { queryClient }>
+      <Todo/>
+    </QueryClientProvider>
   )
 }
 
