271e1c33dddc7486f3d48aa417a4c58d93c90c52
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-15 01:18:51 +0200

    added TodoCard component

---
+++ frontend-reactjs/src/components/TodoCard.tsx
@@ -0,0 +1,15 @@
+import { todoType } from "./Todo"
+
+type TodoCardProps = {
+    id: string;
+    todoItem: todoType;
+}
+
+export const TodoCard = (props: TodoCardProps) => {
+
+  return (
+
+    <div>TodoCard</div>
+
+  )
+}
