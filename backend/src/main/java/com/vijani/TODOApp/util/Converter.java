451e20f757ae27efad98c37da4b57d13e4a2b5e9
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-14 23:27:32 +0200

    added methods to convert into response dtos

--- backend/src/main/java/com/vijani/TODOApp/util/Converter.java
+++ backend/src/main/java/com/vijani/TODOApp/util/Converter.java
@@ -1,11 +1,28 @@
 package com.vijani.TODOApp.util;
 
 import com.vijani.TODOApp.dto.TodoItemRequestDto;
+import com.vijani.TODOApp.dto.TodoItemResponseDto;
 import com.vijani.TODOApp.entity.TodoItem;
+
+import java.util.List;
 
 public class Converter {
 
     public static TodoItem fromTodoItemRequestDto(TodoItemRequestDto dto) {
         return TodoItem.createTodoWithDefaults(dto.title());
+    }
+
+    public static TodoItemResponseDto toTodoItemResponseDto(TodoItem item) {
+        return new TodoItemResponseDto(
+                item.getId(),
+                item.getTitle(),
+                item.getAddedDate(),
+                item.getStatus()
+        );
+    }
+
+    public static List<TodoItemResponseDto> toTodoItemResponseDtoList(List<TodoItem> items) {
+        List<TodoItemResponseDto> todoItemResponses = items.stream().map(item -> toTodoItemResponseDto(item)).toList();
+        return todoItemResponses;
     }
 }
