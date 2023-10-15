7f3b060ba60651d3ce06d9046de41c02d7df96a8
vijanipiyawardana <vijanipiyawardana@gmail.com>
2023-10-15 21:52:58 +0200

    completed addTodo using react-query

--- frontend-reactjs/src/components/Todo.tsx
+++ frontend-reactjs/src/components/Todo.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { TodoCard } from "./TodoCard";
 import axios from 'axios';
+import { useState } from "react";
 
 export type TodoType = {
     id: string;
@@ -9,26 +10,56 @@ export type TodoType = {
     status: string;
 }
 
+export type AddTodoType = {
+    title: string;
+}
+
 export const Todo = () => {
+
+    const queryClient = useQueryClient();
+    const [titleText, setTitleText] = useState("");
 
     const fetchTodos = async () => {
         const response = await axios.get<TodoType[]>("http://localhost:8080/api/todo");
         return response.data;
     }
 
+    const addTodo = async (data: AddTodoType) => {
+        const response = await axios.post("http://localhost:8080/api/todo", data);
+        return response.data;
+    }
+
     const { data, isLoading } = useQuery({
-        queryFn: () => fetchTodos(),
+        queryFn: fetchTodos,
         queryKey: ["todos"]
+    });
+
+    const { mutateAsync: addTodoMutation } = useMutation({
+        mutationFn: addTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries(["todos"]);
+        }
     });
 
     if (isLoading) {
         return <div>Loading...</div>
     }
-    
+
     return (
         <div>
-            { data?.map((todo: TodoType) => {
-                return <TodoCard key={todo.id} todoItem={todo}/>
+            <div>
+                <input type="text" onChange={(e) => setTitleText(e.target.value)} value={titleText} />
+                <button onClick={async () => {
+                    try{
+                        await addTodoMutation({title: titleText});
+                        setTitleText("");
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }}>Add Todo</button>
+            </div>
+            {data?.map((todo: TodoType) => {
+                return <TodoCard key={todo.id} todoItem={todo} />
             })}
         </div>
     )
